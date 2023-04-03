c83a7d15f78187df0f2981c7f9f5cfa2f05efdce
A1dik <114150938+A1dik@users.noreply.github.com>
2023-04-03 21:24:19 +0600

    lab 8

--- lab8/paint/main.py
+++ lab8/paint/main.py
@@ -1,108 +1,106 @@
-from turtle import position
-import math
 import pygame
 
+pygame.init()
+running = True
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((640, 480))
-    baseLayer = pygame.Surface((640, 480))
-    clock = pygame.time.Clock()
-    R = (255, 0, 0)
-    G = (0, 255, 0)
-    B = (0, 0, 255)
-    key_R = True
-    key_G = False
-    key_B = False
+WINDOW_SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT) = (1000, 600)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 
-    prevX = 0
-    prevY = 0
+screen = pygame.display.set_mode(WINDOW_SIZE)
 
-    currentX = -1
-    currentY = -1
+color = BLACK
+shape = 'line'
 
-    screen.fill((0, 0, 0))
+clock = pygame.time.Clock()
+fps = 60
 
-    isMouseDown = False
-    rectan = True
-    circle = False
-    eraser = False
-    while True:
+pygame.display.set_caption('uPaint')
+screen.fill(WHITE)
+
+width = 1
+
+prev, cur = None, None
+
+font = pygame.font.SysFont('Verdana', 15)
+
+pygame.draw.line(screen, pygame.Color('purple'), (0, 31), (WINDOW_WIDTH, 31), 5)
+
+
+while running:
+    pygame.draw.rect(screen, WHITE, (0, 0, WINDOW_WIDTH, 30))
+    screen.blit(font.render(f'Mode: {shape}', True, BLACK), (10, 10))
+    screen.blit(font.render(f'Width: {width}', True, BLACK), (310, 10))
+    screen.blit(font.render(f'Color: {color}', True, BLACK), (610, 10))
+    for event in pygame.event.get():
         pressed = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+        ctrl_pressed = pressed[pygame.K_RCTRL] or pressed[pygame.K_LCTRL]
+        alt_pressed = pressed[pygame.K_RALT] or pressed[pygame.K_LALT]
+
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if pressed[pygame.K_DOWN] and width > 1:
+                width -= 1
+            if pressed[pygame.K_UP]:
+                width += 1
+            if alt_pressed and pressed[pygame.K_b]:
+                color = BLUE
+            if alt_pressed and pressed[pygame.K_r]:
+                color = RED
+            if alt_pressed and pressed[pygame.K_g]:
+                color = GREEN
+            if alt_pressed and pressed[pygame.K_p]:
+                color = pygame.Color('purple')
+            if alt_pressed and pressed[pygame.K_o]:
+                color = pygame.Color('orange')
+            if alt_pressed and pressed[pygame.K_i]:
+                color = pygame.Color('indigo')
+            if ctrl_pressed and pressed[pygame.K_c]:
+                shape = 'circle'
+            if ctrl_pressed and pressed[pygame.K_r]:
+                shape = 'rectangle'
+            if ctrl_pressed and pressed[pygame.K_l]:
+                shape = 'line'
+            if ctrl_pressed and pressed[pygame.K_e]:
+                shape = 'eraser'
+
+        if shape == 'line' or shape == 'eraser':
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    isMouseDown = True
-                    currentX = event.pos[0]
-                    currentY = event.pos[1]
-                    prevX = event.pos[0]
-                    prevY = event.pos[1]
-            if event.type == pygame.MOUSEBUTTONUP:
-                isMouseDown = False
-                baseLayer.blit(screen, (0, 0))
+                prev = pygame.mouse.get_pos()
             if event.type == pygame.MOUSEMOTION:
-                if isMouseDown:
-                    currentX = event.pos[0]
-                    currentY = event.pos[1]
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    rectan = True
-                    circle = False
-                    eraser = False
-                if event.key == pygame.K_2:
-                    rectan = False
-                    circle = True
-                    eraser = False
-                if event.key == pygame.K_3:
-                    rectan = False
-                    circle = False
-                    eraser = True
-                if event.key == pygame.K_r:
-                    key_R = True
-                    key_G = False
-                    key_B = False
-                if event.key == pygame.K_g:
-                    key_R = False
-                    key_G = True
-                    key_B = False
-                if event.key == pygame.K_b:
-                    key_R = False
-                    key_G = False
-                    key_B = True
+                cur = pygame.mouse.get_pos()
+                if prev:
+                    if shape == 'line':
+                        pygame.draw.line(screen, color, prev, cur, width)
+                    if shape == 'eraser':
+                        pygame.draw.line(screen, WHITE, prev, cur, width)
+                    prev = cur
+            if event.type == pygame.MOUSEBUTTONUP:
+                prev = None
+        else:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                prev = pygame.mouse.get_pos()
+            if event.type == pygame.MOUSEBUTTONUP:
+                cur = pygame.mouse.get_pos()
+                if shape == 'circle':
+                    x = (prev[0]+cur[0])/2
+                    y = (prev[1]+cur[1])/2
+                    rx = abs(prev[0]-cur[0])/2
+                    ry = abs(prev[1]-cur[1])/2
+                    r = (rx + ry)/2
+                    pygame.draw.circle(screen, color, (x, y), r, width)
+                if shape == 'rectangle':
+                    x = min(prev[0], cur[0])
+                    y = min(prev[1], cur[1])
+                    lx = abs(prev[0]-cur[0])
+                    ly = abs(prev[1]-cur[1])
+                    pygame.draw.rect(screen, color, (x, y, lx, ly), width)
+    pygame.display.flip()
+    clock.tick(fps)
 
-        if eraser:
-            if isMouseDown:  # eraser
-                pygame.draw.line(screen, (0, 0, 0), (prevX, prevY), (currentX, currentY), 10)
-
-            prevX = currentX
-            prevY = currentY
-        elif isMouseDown and prevX != -1 and prevY != -1 and currentX != -1 and currentY != -1:
-            screen.blit(baseLayer, (0, 0))
-
-            if circle:  # circle
-                radius = math.sqrt((abs(prevX - currentX)) ** 2 + (abs(prevY - currentY)) ** 2)
-                if key_R:
-                    pygame.draw.circle(screen, R, (min(prevX, currentX), min(prevY, currentY)), radius, 1)
-                elif key_G:
-                    pygame.draw.circle(screen, G, (min(prevX, currentX), min(prevY, currentY)), radius, 1)
-                elif key_B:
-                    pygame.draw.circle(screen, B, (min(prevX, currentX), min(prevY, currentY)), radius, 1)
-            elif rectan:  # rectangle
-                r = calculateRect(prevX, prevY, currentX, currentY)
-                if key_R:
-                    pygame.draw.rect(screen, R, pygame.Rect(r), 1)
-                elif key_G:
-                    pygame.draw.rect(screen, G, pygame.Rect(r), 1)
-                elif key_B:
-                    pygame.draw.rect(screen, B, pygame.Rect(r), 1)
-        pygame.display.flip()
-        clock.tick(60)
-
-
-def calculateRect(x1, y1, x2, y2):
-    return pygame.Rect(min(x1, x2), min(y1, y2), abs(x1 - x2), abs(y1 - y2))
-
-
-main()
+pygame.quit()
